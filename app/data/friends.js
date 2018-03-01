a8e6629264186736eece428bd22fdf36ddab66e0
Christopher Biessener <cpbiessener@gmail.com>
2018-03-01 15:19:20 -0600

    html cleanup; form validation

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,17 @@ var friend_data = [
 {
   "name": "Roy Trenneman",
   "photo": "../data/itc_roy.jpg",
-  "scores": [ 1,1,1,1,1,1,1,1,1,1 ]
+  "scores": [ 1,2,5,1,5,1,1,5,5,1 ]
 },
 {
   "name": "Maurice Moss",
   "photo": "../data/itc_moss.jpg",
-  "scores": [ 3,3,3,3,3,3,3,3,3,3 ]
+  "scores": [ 3,5,1,4,4,4,1,3,2,2 ]
 },
 {
   "name": "Jen Barber",
   "photo": "../data/itc_jen.jpg",
-  "scores": [ 5,5,5,5,5,5,5,5,5,5 ]
+  "scores": [ 2,3,4,3,2,2,5,4,4,4 ]
 }
 ];
 
